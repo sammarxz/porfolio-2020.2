7453007eff6f0a71f806a53da540c75240ac4d6e
Sam Marxz <sammarxzz@gmail.com>
2020-08-20 09:54:52 -0300

    Add Pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "normalize.css"
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World</h1>
+    <Link to="/about">About me</Link>
+  </div>
+)
+
+export default IndexPage

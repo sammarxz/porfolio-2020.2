1dd96619615432e7489e107cee1e53d34208baed
Sam Marxz <sammarxzz@gmail.com>
2020-09-04 11:15:58 -0300

    Test

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -10,25 +10,29 @@ import Footer from '../components/footer'
 // Styles
 import '../styles/styles.scss'
 
-const Default = ({ headerRef, children }) => (
-  <>
-    <Header headerRef={headerRef} />
-    {isMobile ? (
-      <div className="container p--relative">
-        <Intro />
-        {children}
-        <Footer />
-      </div>
-    ) : (
-      <SmoothScroll>
+const Default = ({ headerRef, children }) => {
+  console.log(isMobile)
+
+  return (
+    <>
+      <Header headerRef={headerRef} />
+      {isMobile ? (
         <div className="container p--relative">
           <Intro />
           {children}
           <Footer />
         </div>
-      </SmoothScroll>
-    )}
-  </>
-)
+      ) : (
+        <SmoothScroll>
+          <div className="container p--relative">
+            <Intro />
+            {children}
+            <Footer />
+          </div>
+        </SmoothScroll>
+      )}
+    </>
+  )
+}
 
 export default Default

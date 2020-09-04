679b3f79ed728221c95c3f391ee66ef1a3add7fd
Sam Marxz <sammarxzz@gmail.com>
2020-09-04 11:57:47 -0300

    Change to use social media in Sanity.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,49 +1,34 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const Footer = () => {
-  const links = [
+  const links = useStaticQuery(graphql`
     {
-      name: 'Instagram',
-      url: 'https://instagram.com/sammarques55'
-    },
-    {
-      name: 'Dribbble',
-      url: 'https://dribbble.com/sammarxz'
-    },
-    {
-      name: 'Github',
-      url: 'https://github.com/sammarxz'
-    },
-    {
-      name: 'Linkedin',
-      url: 'https://linkedin.com/in/sammarxz'
-    },
-    {
-      name: 'Medium',
-      url: 'https://medium.com/@sammarxz'
-    },
-    {
-      name: 'YouTube',
-      url: 'https://youtube.com/user/Nerdezinhu'
+      sanityAuthor {
+        social {
+          name
+          url
+        }
+      }
     }
-  ]
+  `)
+
+  const { social } = links.sanityAuthor
 
   return (
     <footer className="footer mb--x-big d--grid row ai--end fd--column">
       <p>©2020 Samuel Marxz.</p>
       <p>
-        Made with
-        {' '}
+        Made with{' '}
         <a href="https://www.gatsbyjs.com/" target="blank" className="link">
           <span role="img" aria-label="atom">
             ⚛️
-          </span>
-          {' '}
+          </span>{' '}
           GatsbyJS
         </a>
       </p>
       <ul className="footer__social col--1-5">
-        {links.map(({ name, url }) => (
+        {social.map(({ name, url }) => (
           <li key={name} className="pb--x-small mr--small">
             <a href={url} target="blank" className="link">
               {name}
